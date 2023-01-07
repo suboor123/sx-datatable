7f8b0ccdbdc7e9c1a43edd992cee4aeab09372fc
suboor khan <suboorkhan@suboors-MacBook-Air.local>
2023-01-07 23:12:15 +0530

    init create-react-library@3.1.1

---
+++ example/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'sx-datatable'
+import 'sx-datatable/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
